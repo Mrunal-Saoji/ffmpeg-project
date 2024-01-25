faa15d0d94c4e9e7e6112fef378f836aaaaa9825
Mrunal Saoji <mrunalsaoji@gmail.com>
2024-01-25 04:13:37 +0000

    working on ffmpeg

--- index.js
+++ index.js
@@ -33,8 +33,34 @@ const jobs = {
 
 };
 
-app.post('/convert',upload.single('filename'),(req,res) => {
-    console.log(req.file.path)
+app.post('/convert',upload.single('filename'),async(req,res) => {
+    const jobId = Date.now().toString();
+
+    const inputFilePath = req.file.path;
+    videoFormat = req.body.videoFormat;
+    const outputFilePath = `converted_${jobId}.${videoFormat}`
+
+    //ffmpeg
+
+    const {streams} = await ffprobe(inputFilePath,{
+        path: "ffprobe"
+    })
+
+    const videoStream = streams.find((stream) => stream.codec_type === "video")
+
+    const totalTimeDuration = videoStream ? parseFloat(videoStream.duration) : 0
+
+    console.log(totalTimeDuration)
+
+    //making progress Bar
+    const bar = new Progress("[:bar] :percent :etas",{
+        width: 40,
+        total:totalTimeDuration
+    })
+
+    const job = ffmpeg()
+                .input(inputFilePath)
+                .videoCodec("")
 })
 
 
